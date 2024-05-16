711eb96be7f00abe8c7672360fb9f366f0d2e0be
yusubhin <sdbtnqlss@gmail.com>
2024-05-16 15:24:47 +0900

    practice #JDBC connection

---
+++ jdbc-practices/src/main/java/test/ConnectionTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//import org.mariadb.jdbc.Connection;
+
+public class ConnectionTest {
+	public static void main(String[] args) {
+		Connection connection = null;
+		
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			//2. 연결하기
+			String url = "jdbc:mariadb://192.168.0.193:3306/webdb?charset=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			System.out.println("success!!");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실퍠: " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if(connection != null) {
+					connection.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+}

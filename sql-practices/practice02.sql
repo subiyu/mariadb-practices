3c7292d255f060699adf6a9eca15ca69fd05ffb3
yusubhin <sdbtnqlss@gmail.com>
2024-05-16 13:04:12 +0900

    practice sql #subquery

--- sql-practices/practice02.sql
+++ sql-practices/practice02.sql
@@ -1,21 +1,37 @@
 -- 집계(통계) SQL 문제입니다.
 
 -- 문제 1. 
--- 최고임금(salary)과  최저임금을 “최고임금, “최저임금”프로젝션 타이틀로 함께 출력해 보세요. 두 임금의 차이는 얼마인가요? 함께 “최고임금 – 최저임금”이란 타이틀로 출력해 보세요.
+-- 최고임금(salary)과  최저임금을 “최고임금, “최저임금”프로젝션 타이틀로 함께 출력해 보세요. 
+-- 두 임금의 차이는 얼마인가요? 함께 “최고임금 – 최저임금”이란 타이틀로 출력해 보세요.
+SELECT max(salary) as '최고임금', min(salary) as '최저임금', (max(salary)-min(salary)) as '최고임금-최저임금'
+FROM salaries;
 
 -- 문제2.
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
+SELECT date_format(max(hire_date), '%Y년 %m월 %d일')
+FROM employees;
 
 -- 문제3.
 -- 가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
+SELECT date_format(min(hire_date), '%Y년 %m월 %d일')
+FROM employees;
+
 
 -- 문제4.
 -- 현재 이 회사의 평균 연봉은 얼마입니까?
+SELECT avg(salary) as '평균연봉'
+FROM salaries
+WHERE to_date = '9999-01-01';
 
 -- 문제5.
 -- 현재 이 회사의 최고/최저 연봉은 얼마입니까?
+SELECT max(salary) as '최고연봉', min(salary) as '최저연봉'
+FROM salaries;
 
 -- 문제6.
 -- 최고 어린 사원의 나이와 최 연장자의 나이는?
+SELECT truncate((to_days(now()) - to_days(min(birth_date))) / 365, 0) as '최 연장자 나이',
+	   truncate((to_days(now()) - to_days(max(birth_date))) / 365, 0) as '최고 어린 사원 나이'
+FROM employees;

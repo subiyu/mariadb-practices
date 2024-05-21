1ccbfd712d174bfc57355fc717236f7bbabad8d5
yusubhin <sdbtnqlss@gmail.com>
2024-05-21 17:32:47 +0900

    update webdb

---
+++ bin/jdbc-practices/sql/webdb.sql
@@ -0,0 +1,11 @@
+-- insert
+INSERT INTO dept values(null, '디자인팀');
+
+-- delete
+DELETE FROM dept WHERE no = 7;
+
+-- select
+SELECT * FROM dept;
+
+-- update
+UPDATE dept set name='시스템개발' WHERE no=2;
